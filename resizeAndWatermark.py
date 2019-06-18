eb4b8421227b6971d7764679a3d3cd372c848ad1
briskCZ <briskcz@gmail.com>
2019-06-18 13:09:24 +0200

    Added option for selecting side of the picture that watermark will be on

--- resizeAndWatermark.py
+++ resizeAndWatermark.py
@@ -3,14 +3,13 @@
 from gimpfu import *
 import math, os
 
-def do_stuff(img, drw, wtm_toggle, w_offset, h_offset, path, rsz_toggle, exp_toggle, exp_path):
+def do_stuff(img, drw, wtm_toggle, side_radio, w_offset, h_offset, path, rsz_toggle, exp_toggle, exp_path):
 	pdb.gimp_undo_push_group_start(img)
 		
 	img_width = pdb.gimp_image_width(img)
 	img_height = pdb.gimp_image_height(img)
 	
 	if wtm_toggle == 1:
-		#pridat zadavani rohu
 		try:
 			watermark = pdb.gimp_file_load_layer(img,path)
 
@@ -18,8 +17,13 @@ def do_stuff(img, drw, wtm_toggle, w_offset, h_offset, path, rsz_toggle, exp_tog
 
 			wtm_width = pdb.gimp_drawable_width(watermark)
 			wtm_height = pdb.gimp_drawable_height(watermark)
+			
+			if(side_radio == 0):
+				watermark = pdb.gimp_item_transform_2d(watermark, wtm_width, wtm_height, 1, 1, 0, img_width - w_offset, img_height - h_offset)
+			if(side_radio == 1):
+				watermark = pdb.gimp_item_transform_2d(watermark, wtm_width, wtm_height, 1, 1, 0, w_offset + wtm_width, img_height - h_offset)
 
-			watermark = pdb.gimp_item_transform_2d(watermark, wtm_width, wtm_height, 1, 1, 0, img_width - w_offset, img_height - h_offset)
+			
 			drw = pdb.gimp_image_flatten(img)
 			
 		except Exception as e:
@@ -62,7 +66,13 @@ register(
 		(PF_IMAGE, "img", "Input image", None),
 		(PF_DRAWABLE, "drw", "Input layer", None),
 		(PF_TOGGLE, "wtm_toggle", "Add watermark.", 0),
-		(PF_INT, "w_offset", "Offset of watermark from the right side.", 25),
+		(PF_RADIO, "side_radio", "Side: ", 0,
+			(
+				("Right", 0),
+				("Left", 1)
+			)
+		),
+		(PF_INT, "w_offset", "Offset of watermark from the side.", 25),
 		(PF_INT, "h_offset", "Offset of watermark from the bottom.", 30),
 		(PF_FILE, "path", "Watermark file:", ""),
 		(PF_TOGGLE, "rsz_toggle", "Resize.", 1),
